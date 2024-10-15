a1409290c5ab7bf02bbb5556a4540b1251725d21
Rahul Thapa <rahulthapaofficial@gmail.com>
2024-10-15 18:08:28 +0545

    Create Account Link Changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ export default async function Header() {
           {!session && (
             <>
               <Link href={"/login"}>Sign In</Link>
-              <Link href={"/register"}>Create Account</Link>
+              <Link href={"/login"}>Create Account</Link>
             </>
           )}
         </nav>
