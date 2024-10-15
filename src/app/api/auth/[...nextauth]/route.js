c80e1715de22021aadc10b6e0b96e448e854a906
Rahul Thapa <rahulthapaofficial@gmail.com>
2024-10-15 10:04:11 +0545

    Next auth Bug Fixed

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import client from "@/lib/db";
 
-const authOptions = {
+export const authOptions = {
   secret: process.env.SECRET,
   adapter: MongoDBAdapter(client),
   // Configure one or more authentication providers
